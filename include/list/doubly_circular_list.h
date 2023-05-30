36faca9a7d102217336f46910b4598a3a93a2b8d
Fredson <fredsonchaves07@gmail.com>
2023-05-30 19:02:45 -0300

    feat: init config circular list

---
+++ include/list/doubly_circular_list.h
@@ -0,0 +1,8 @@
+# ifndef DOUBLY_CIRCULAR_LIST
+# define DOUBLY_CIRCULAR_LIST
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+#endif
---
+++ test/datastructures/list/test_doubly_circular_list.c
@@ -0,0 +1,8 @@
+# include <string.h>
+
+# include "../../../include/list/doubly_circular_list.h"
+# include "../../test_app.h"
+
+void run_test_doubly_circular_list() {
+
+}
