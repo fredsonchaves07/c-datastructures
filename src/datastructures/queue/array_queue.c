33d8fed8558f56bf3cf0bc3a7084a8f5b1cc3c1f
Fredson <fredsonchaves07@gmail.com>
2023-07-18 09:25:58 -0300

    feat: finish implementation array queue functions

--- src/datastructures/queue/array_queue.c
+++ src/datastructures/queue/array_queue.c
@@ -7,6 +7,7 @@ struct _array_queue {
     size_t count;
     size_t data_size;
     size_t capacity;
+    size_t top;
     void *elements[10];
 };
 
@@ -14,6 +15,8 @@ ArrayQueue *array_queue_create(size_t data_size) {
     ArrayQueue *queue = calloc(1, sizeof(ArrayQueue));
     queue->data_size = data_size;
     queue->capacity = 10;
+    queue->top = 0;
+    queue->count = 0;
     return queue;
 }
 
@@ -42,18 +45,68 @@ void _array_queue_increase_capacity(ArrayQueue *queue) {
     *queue->elements = *new_elements;
 }
 
-void array_queue_enqueue(ArrayQueue *queue, void *element);
+void array_queue_enqueue(ArrayQueue *queue, void *element) {
+    if (_array_queue_is_full(queue))
+        _array_queue_increase_capacity(queue);
+    for (int i = queue->capacity - 1; i >= 0; i --) {
+        if (queue->elements[i] != NULL) {
+            queue->elements[i + 1] = element;
+            break;
+        }
+        if (queue->elements[i] == NULL && i == 0) {
+            queue->elements[i] = element;
+        }
+    }
+    queue->count += 1;
+}
 
-void *array_queue_dequeue(ArrayQueue *queue);
+void *array_queue_dequeue(ArrayQueue *queue) {
+    if (array_queue_is_empty(queue)) return NULL;
+    void *element = queue->elements[queue->top];
+    queue->elements[queue->top] = NULL;
+    queue->top += 1;
+    queue->count -= 1;
+    return element;
+}
 
-void *array_queue_peek(ArrayQueue *queue);
+void *array_queue_peek(ArrayQueue *queue) {
+    if (array_queue_is_empty(queue)) return NULL;
+    return queue->elements[queue->top];
+}
 
-bool array_queue_is_empty(const ArrayQueue *queue);
+bool array_queue_is_empty(const ArrayQueue *queue) {
+    return queue->count == 0;
+}
 
-size_t array_queue_length(const ArrayQueue *queue);
+size_t array_queue_length(const ArrayQueue *queue) {
+    return queue->count;
+}
 
-char *array_queue_to_string(const ArrayQueue *queue);
+char *array_queue_to_string(const ArrayQueue *queue) {
+    if (array_queue_is_empty(queue))
+        return "[]";
+    char *array_queue_data = calloc(1, queue->data_size * sizeof(char *));
+    strcat(array_queue_data, "[");
+    for (int i = 0; i < queue->capacity; i++) {
+        if (queue->elements[i] != NULL) {
+            if (strlen(array_queue_data) > 1)
+                strcat(array_queue_data, ", ");
+            strcat(array_queue_data, queue->elements[i]);
+        }
+    }
+    return strcat(array_queue_data, (char *) "]");
+}
 
-void array_queue_free(ArrayQueue **queue);
+void array_queue_free(ArrayQueue **queue) {
+    ArrayQueue *stack_ref = *queue;
+    array_queue_clear(*queue);
+    free(stack_ref);
+    queue = NULL;
+}
 
-void array_queue_clear(ArrayQueue *queue);
+void array_queue_clear(ArrayQueue *queue) {
+    *queue->elements = NULL;
+    queue->count = 0;
+    queue->capacity = 0;
+    queue->data_size = 0;
+}
